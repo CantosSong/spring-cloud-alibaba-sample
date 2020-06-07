bcc553b1e788056366fd009a6562a3181b8279a0
mengmeng.song <mengmeng.song@shangri-la.com>
2020-06-07 20:08:15 +0800

    init project # add project gateway-sample

---
+++ gateway-sample/src/test/java/com/github/cantos/gateway/GatewaySampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.cantos.gateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GatewaySampleApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
