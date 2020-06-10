3aa68f5c0ceb0eb0b0afffd2d08404d4a6b28316
mengmeng.song <mengmeng.song@shangri-la.com>
2020-06-10 10:02:09 +0800

    还原页面登陆

--- gateway-sample/src/main/java/com/github/cantos/gateway/config/SecurityConfig.java
+++ gateway-sample/src/main/java/com/github/cantos/gateway/config/SecurityConfig.java
@@ -1,29 +1,18 @@
 package com.github.cantos.gateway.config;
 
 import org.springframework.context.annotation.Bean;
-import org.springframework.http.HttpStatus;
 import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
 import org.springframework.security.config.web.server.ServerHttpSecurity;
 import org.springframework.security.web.server.SecurityWebFilterChain;
 import org.springframework.security.web.server.csrf.CookieServerCsrfTokenRepository;
-import reactor.core.publisher.Mono;
 
 @EnableWebFluxSecurity
 public class SecurityConfig {
     @Bean
     public SecurityWebFilterChain springSecurityFilterChain(ServerHttpSecurity http) {
-        String[] patterns = new String[]{"/login"};
-        return http.csrf(csrf -> csrf.csrfTokenRepository(CookieServerCsrfTokenRepository.withHttpOnlyFalse()))
-                .exceptionHandling()
-                .authenticationEntryPoint((swe, e) -> Mono.fromRunnable(() -> {
-                    swe.getResponse().setStatusCode(HttpStatus.UNAUTHORIZED);
-                })).accessDeniedHandler((swe, e) -> Mono.fromRunnable(() -> {
-                    swe.getResponse().setStatusCode(HttpStatus.FORBIDDEN);
-                })).and()
-                .authorizeExchange()
-                .pathMatchers(patterns).permitAll()
-                .anyExchange().authenticated()
-                .and()
+        return http.
+                csrf(csrf -> csrf.csrfTokenRepository(CookieServerCsrfTokenRepository.withHttpOnlyFalse()))
+                .formLogin().and()
                 .build();
     }
 }
